44fd100695c9619d993530f7122cff3c423834ab
Sufiyan Mirza <khansufiyanmirza@gmail.com>
2022-03-24 18:15:35 +0530

    initial commit

---
+++ sample/generator.go
@@ -0,0 +1,8 @@
+package sample
+
+import "github.com/KhanSufiyanMirza/pcbook/sufiyan_pb"
+
+//NewKeyboard returns a new  sample keyboard
+func NewKeyboard() *sufiyan_pb.Keyboard {
+	keyboard := &sufiyan_pb.Keyboard{}
+}
